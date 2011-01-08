2bc5922a21b453c76148bae7ed5d10a12e09f325
Doridian Draconia <mriq91@gmail.com>
2011-01-08 22:29:57 +0000

    git-svn-id: https://svn.doridian.de/foxscreen@2 3dd9b80d-1a31-4757-a113-1aa36071f0d0

---
+++ FoxScreen/frmMain.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace FoxScreen
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.btnFullshot = new System.Windows.Forms.Button();
+            this.tbPword = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbLB = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FTP URI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FTP User:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FTP PW:";
+            // 
+            // tbHost
+            // 
+            this.tbHost.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbHost.Location = new System.Drawing.Point(84, 12);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(294, 20);
+            this.tbHost.TabIndex = 3;
+            this.tbHost.Text = "ftp://example.com";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbUser.Location = new System.Drawing.Point(84, 38);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(294, 20);
+            this.tbUser.TabIndex = 4;
+            this.tbUser.Text = "anonymous";
+            // 
+            // btnFullshot
+            // 
+            this.btnFullshot.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFullshot.Location = new System.Drawing.Point(12, 155);
+            this.btnFullshot.Name = "btnFullshot";
+            this.btnFullshot.Size = new System.Drawing.Size(366, 33);
+            this.btnFullshot.TabIndex = 6;
+            this.btnFullshot.Text = "Full screenshot";
+            this.btnFullshot.UseVisualStyleBackColor = true;
+            this.btnFullshot.Click += new System.EventHandler(this.btnFullshot_Click);
+            // 
+            // tbPword
+            // 
+            this.tbPword.Location = new System.Drawing.Point(84, 64);
+            this.tbPword.Name = "tbPword";
+            this.tbPword.PasswordChar = '*';
+            this.tbPword.Size = new System.Drawing.Size(294, 20);
+            this.tbPword.TabIndex = 8;
+            this.tbPword.Text = "anonymous";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "HTTP Base:";
+            // 
+            // tbLB
+            // 
+            this.tbLB.Location = new System.Drawing.Point(84, 90);
+            this.tbLB.Name = "tbLB";
+            this.tbLB.Size = new System.Drawing.Size(294, 20);
+            this.tbLB.TabIndex = 10;
+            this.tbLB.Text = "http://screen.example.com/";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "FoxScreen";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(12, 116);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(366, 33);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save credentials";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 194);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbLB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbPword);
+            this.Controls.Add(this.btnFullshot);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.tbHost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "FoxScreen (c) by Doridian 2011";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.Button btnFullshot;
+        private System.Windows.Forms.MaskedTextBox tbPword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbLB;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ FoxScreen/frmMain.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.IO;
+
+namespace FoxScreen
+{
+    public partial class frmMain : Form
+    {
+        KeyboardHook hook = new KeyboardHook();
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            hook.RegisterHotKey(0, Keys.PrintScreen);
+            hook.RegisterHotKey(ModifierKeysH.Alt, Keys.PrintScreen);
+
+            try
+            {
+                string[] lines = File.ReadAllLines("config.cfg");
+                tbHost.Text = lines[0];
+                tbUser.Text = lines[1];
+                tbPword.Text = lines[2];
+                tbLB.Text = lines[3];
+            }
+            catch { }
+        }
+
+        void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            if (e.Modifier == ModifierKeysH.Alt)
+            {
+                CurWndScreenShot();
+            }
+            else
+            {
+                CompleteScreenShot();
+            }
+        }
+
+        public void CompleteScreenShot()
+        {
+            Size allScreensSize = new Size(0, 0);
+            foreach (Screen scr in Screen.AllScreens)
+            {
+                allScreensSize += scr.Bounds.Size;
+            }
+            AreaScreenShot(0, 0, allScreensSize);
+        }
+
+        public void CurWndScreenShot()
+        {
+            IntPtr ahWnd = GetForegroundWindow();
+            RECT rect;
+            GetWindowRect(ahWnd, out rect);
+            AreaScreenShot(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top, GetWindowTitle(ahWnd));
+        }
+        public void AreaScreenShot(int x, int y, int width, int height)
+        {
+            AreaScreenShot(x, y, width, height, "FoxScreen");
+        }
+        public void AreaScreenShot(int x, int y, int width, int height, string customname)
+        {
+            AreaScreenShot(x, y, new Size(width, height), customname);
+        }
+        public void AreaScreenShot(int x, int y, Size size)
+        {
+            AreaScreenShot(x, y, size, "FoxScreen");
+        }
+        public void AreaScreenShot(int x, int y, Size size, string customname)
+        {
+            if (customname != "FoxScreen") customname = "FS_" + customname;
+
+            int imax = customname.Length;
+            char c;
+            char[] cna = customname.ToCharArray(0, imax);
+            for (int i = 0; i < imax; i++)
+            {
+                c = cna[i];
+                if((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && (c < '0' || c > '9'))
+                {
+                    cna[i] = '_';
+                }
+            }
+            customname = new String(cna);
+
+            Font font = new Font("Arial",(Math.Min(size.Width,size.Height) / 6),FontStyle.Regular,GraphicsUnit.Pixel);
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            format.LineAlignment = StringAlignment.Center;
+            Brush brush = new SolidBrush(Color.FromArgb(128, 255, 0, 255));
+
+            Bitmap b = new Bitmap(size.Width, size.Height);
+            Graphics g = Graphics.FromImage(b);
+            g.CopyFromScreen(x, y, 0, 0, size);
+            g.DrawString("FoxScreen\n(c) Doridian",font,brush,new PointF(size.Width / 2, size.Height / 2),format);
+            g.Flush();
+            g.Dispose();
+            try
+            {
+                customname = customname + "_" + FixTwoChar(DateTime.Now.Day) + "-" + FixTwoChar(DateTime.Now.Month) + "-" + DateTime.Now.Year + "_" + FixTwoChar(DateTime.Now.Hour) + "-" + FixTwoChar(DateTime.Now.Minute) + "-" + FixTwoChar(DateTime.Now.Second) + ".jpg";
+                FtpWebRequest ftpr = (FtpWebRequest)FtpWebRequest.Create(tbHost.Text + "/" + customname);
+                ftpr.Method = WebRequestMethods.Ftp.UploadFile;
+                ftpr.UsePassive = true;
+                ftpr.UseBinary = true;
+                ftpr.Credentials = new NetworkCredential(tbUser.Text, tbPword.Text);
+                Stream str = ftpr.GetRequestStream();
+                b.Save(str, System.Drawing.Imaging.ImageFormat.Jpeg);
+                str.Close();
+                FtpWebResponse resp = (FtpWebResponse)ftpr.GetResponse();
+                resp.Close();
+
+                customname = tbLB.Text + customname;
+                Clipboard.SetText(customname);
+
+                notifyIcon.ShowBalloonTip(1000, "FoxScreen: Screenshot uploaded!", "Saved as: " + customname, ToolTipIcon.Info);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "ERROR!");
+            }
+        }
+
+        private void btnFullshot_Click(object sender, EventArgs e)
+        {
+            CompleteScreenShot();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("config.cfg", tbHost.Text + Environment.NewLine + tbUser.Text + Environment.NewLine + tbPword.Text + Environment.NewLine + tbLB.Text);
+        }
+
+        private string FixTwoChar(int num)
+        {
+            if (num < 10) return "0" + num.ToString();
+            return num.ToString();
+        }
+
+
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendMessage(IntPtr handleWindow, uint message, uint wParam, StringBuilder lParam);
+
+        private static string GetWindowTitle(IntPtr handleWindow)
+        {
+            StringBuilder sb = new StringBuilder(256);
+            SendMessage(handleWindow, 0x000D /*WM_GETTEXT*/, 256, sb);
+            return sb.ToString();
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.Visible) this.Hide();
+            else this.Show();
+        }
+    }
+}
