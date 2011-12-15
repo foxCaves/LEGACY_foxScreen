c7ef6c535f3c6a1fee244d3866d8edc8ab3cfd88
Doridian Draconia <mriq91@gmail.com>
2011-12-15 10:52:09 +0100

    Added accientially forgotten frmDropArea.* files

---
+++ FoxScreen/frmDropArea.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FoxScreen
+{
+    partial class frmDropArea
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbDragDrop = new System.Windows.Forms.Label();
+            this.tmOpacity = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbDragDrop
+            // 
+            this.lbDragDrop.AutoSize = true;
+            this.lbDragDrop.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDragDrop.ForeColor = System.Drawing.Color.Red;
+            this.lbDragDrop.Location = new System.Drawing.Point(112, 104);
+            this.lbDragDrop.Name = "lbDragDrop";
+            this.lbDragDrop.Size = new System.Drawing.Size(87, 37);
+            this.lbDragDrop.TabIndex = 1;
+            this.lbDragDrop.Text = "Drag";
+            // 
+            // tmOpacity
+            // 
+            this.tmOpacity.Enabled = true;
+            this.tmOpacity.Interval = 10;
+            this.tmOpacity.Tick += new System.EventHandler(this.tmOpacity_Tick);
+            // 
+            // frmDropArea
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(311, 244);
+            this.Controls.Add(this.lbDragDrop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmDropArea";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmDropArea";
+            this.TopMost = true;
+            this.Click += new System.EventHandler(this.frmDropArea_Click);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmDropArea_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmDropArea_DragEnter);
+            this.DragLeave += new System.EventHandler(this.frmDropArea_DragLeave);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmDropArea_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbDragDrop;
+        private System.Windows.Forms.Timer tmOpacity;
+
+    }
+}
---
+++ FoxScreen/frmDropArea.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FoxScreen
+{
+    public partial class frmDropArea : Form
+    {
+        UploadOrganizer uploadOrganizer;
+        public frmDropArea(UploadOrganizer uploadOrganizer)
+        {
+            InitializeComponent();
+            targetOpacity = this.Opacity;
+            this.uploadOrganizer = uploadOrganizer;
+        }
+
+        private void frmDropArea_DragLeave(object sender, EventArgs e)
+        {
+            DragEnd();
+        }
+
+        private void frmDropArea_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop) || e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+            DragStart();
+        }
+
+        private void frmDropArea_DragDrop(object sender, DragEventArgs e)
+        {
+            DragEnd();
+
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (files != null)
+            {
+                foreach (string file in files)
+                {
+                    if ((!File.Exists(file)) || Directory.Exists(file)) continue;
+
+                    int fPathPos = file.LastIndexOf('/');
+                    int fPathPos2 = file.LastIndexOf('\\');
+                    if(fPathPos2 > fPathPos) fPathPos = fPathPos2;
+                    string filename = file.Substring(fPathPos + 1);
+
+                    int fPosDot = filename.LastIndexOf('.');
+                    string extension;
+                    if (fPosDot < 0)
+                    {
+                        extension = "bin";
+                    }
+                    else if (fPosDot == 0)
+                    {
+                        extension = filename.Substring(1);
+                        filename = "file";
+                    }
+                    else
+                    {
+                        extension = filename.Substring(fPosDot + 1);
+                        filename = filename.Substring(0, fPosDot);
+                    }
+
+                    MemoryStream mstr = new MemoryStream(File.ReadAllBytes(file));
+                    uploadOrganizer.AddUpload("FS_" + filename, extension, mstr);
+                }
+            }
+
+            string text = e.Data.GetData(DataFormats.Text) as string;
+            if (text != null)
+            {
+                uploadOrganizer.AddUpload("FS_paste", "txt", new MemoryStream(System.Text.Encoding.ASCII.GetBytes(text)));
+            }
+        }
+
+        private void DragStart()
+        {
+            targetOpacity = 1.0;
+            lbDragDrop.Text = "Drop";
+        }
+
+        private void DragEnd()
+        {
+            targetOpacity = 0.5;
+            lbDragDrop.Text = "Drag";
+        }
+
+        public void ToggleVisibility()
+        {
+            if (this.targetOpacity <= 0 || !this.Visible)
+            {
+                this.targetOpacity = 0.5;
+                if (!this.Visible)
+                    this.Show();
+            }
+            else
+            {
+                this.targetOpacity = 0.0;
+            }
+        }
+
+        private double targetOpacity;
+        private void tmOpacity_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity > targetOpacity)
+            {
+                this.Opacity -= 0.01;
+            }
+            else if (this.Opacity < targetOpacity)
+            {
+                this.Opacity += 0.01;
+            }
+            else
+            {
+                if (this.Opacity <= 0)
+                    this.Hide();
+
+                return;
+            }
+        }
+
+        private void frmDropArea_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            g.DrawRectangle(Pens.Red, 0, 0, this.Width - 1, this.Height - 1);
+            g.DrawRectangle(Pens.Red, 1, 1, this.Width - 3, this.Height - 3);
+        }
+
+        private void frmDropArea_Click(object sender, EventArgs e)
+        {
+            this.ToggleVisibility();
+        }
+    }
+}
